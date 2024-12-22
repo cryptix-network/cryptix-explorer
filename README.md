b41141c1e8992611ecac1f05a545530c042d3873
Cryptix Network <info@king-krypto.de>
2024-12-22 12:02:30 +0100

    Release

---
+++ README.md
@@ -0,0 +1,25 @@
+# CRYPTIX EXPLORER
+
+This is the source code for Cryptix Explorer [https://explorer.cryptix.org](https://explorer.cryptix.org) written in JS with React.JS library.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Development
+
+For developing this you need Node.JS and just npm install.
+
+
+## Start
+
+Start with a Bat File / Powershell file or similar:
+
+## Bat:
+
+set REACT_APP_API_SERVER="http://127.0.0.1:8000"
+set REACT_APP_WS_SERVER="http:////127.0.0.1:8001"
+set REACT_APP_VERCEL_GIT_COMMIT_SHA="v1.8"
+set PORT=80 && npm start --verbose
+
+
+
+
---
+++ src/explorer_constants.js
@@ -0,0 +1,39 @@
+let SOCKET_SERVER = process.env.WS_SERVER || "http://localhost:8001";
+let SUFFIX = ""
+let API_SERVER = process.env.REACT_APP_API_SERVER || "http://localhost:8000"
+let ADDRESS_PREFIX = "cryptix:";
+let CRYPTIX_UNIT = "CYTX";
+
+let BPS = 1
+
+switch (process.env.REACT_APP_NETWORK) {
+    case "testnet-10":
+        SOCKET_SERVER = "http://localhost:8000";
+        ADDRESS_PREFIX = "cryptixtest:"
+        if (!API_SERVER) {
+            API_SERVER = "http://localhost:8001"
+        }
+        SUFFIX = " TN10"
+        KASPA_UNIT = "TCYTX"
+        break;
+    case "testnet-11":
+        SOCKET_SERVER = "http://localhost:8000";
+        ADDRESS_PREFIX = "cryptixtest:"
+        if (!API_SERVER) {
+            API_SERVER = "http://localhost:8001"
+        }
+        SUFFIX = " TN11"
+        KASPA_UNIT = "TCYTX"
+        BPS = 10
+        break;
+
+    // mainnet
+    default:
+        SOCKET_SERVER = "http://localhost:8000";
+        if (!API_SERVER) {
+            API_SERVER = "http://localhost:8001"
+        }
+        break;
+}
+
+export { SOCKET_SERVER, SUFFIX, API_SERVER, ADDRESS_PREFIX, BPS, CRYPTIX_UNIT }
